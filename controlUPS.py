affdc769ffe55762dbffb65488959bf7f9d80da3
Arabadann <adrianrabadan@yahoo.com.mx>
2024-05-31 20:34:01 +0000

    Update final files

--- controlUPS.py
+++ controlUPS.py
@@ -1,5 +1,5 @@
 """
-Description: This script provides classes for controlling a UPS USB device using NUT UPS on a linux system.
+Description: This script provides the class for controlling a UPS USB device using NUT on a Linux system.
 
 Author: Adrian Rabadan Ortiz
 
@@ -9,9 +9,10 @@ Date Modified: Month DD, Year
 
 Version: 1.0
 
-Dependencies: NUT
-
-"""
+Dependencies: 
+    NUT (using driver blazer_usb for Cypress Semiconductor 0665:5161)
+    ups.conf (offdelay)
+    """
 
 import subprocess
 
@@ -27,7 +28,8 @@ class UPSManager:
     
     def __init__(self, ups_name, user, password):
         """
-        Initializes the UPSManager class with the given UPS name, username, and password.
+        Initializes the UPSManager class with the given UPS name, username, password, 
+        shutdown delay, and restart delay.
 
         Args:
             ups_name (str): Name of the UPS device.
@@ -38,6 +40,21 @@ class UPSManager:
         self.user = user
         self.password = password
 
+
+    def restart_nut_services(self):
+        """
+        Restarts the NUT services (nut-server and nut-monitor).
+
+        Returns:
+            str: Success message if services restarted successfully, or an error message otherwise.
+        """
+        try:
+            subprocess.run(["sudo", "systemctl", "restart", "nut-server"], check=True)
+            subprocess.run(["sudo", "systemctl", "restart", "nut-monitor"], check=True)
+            return "NUT services restarted successfully."
+        except subprocess.CalledProcessError as e:
+            return f"An error occurred: {e.stderr.decode('utf-8')}"
+    
     def _run_command(self, command):
         """
         Runs a shell command and returns the output.
@@ -53,25 +70,75 @@ class UPSManager:
             return result.stdout.decode('utf-8')
         except subprocess.CalledProcessError as e:
             return f"An error occurred: {e.stderr.decode('utf-8')}"
-
-    def shutdown(self):
+    
+    def list_commands(self):
         """
-        Shuts down the UPS device by sending the appropriate command.
+        Lists available commands for the UPS device.
+
+        Returns:
+            str: The list of available commands.
+        """
+        command = f"upscmd -l {self.ups_name}"
+        return self._run_command(command)
+
+    def reboot_load(self):
+        """
+        Reboots the UPS device after a defined delay (offdelay) and restarting again.
+
+        Returns:
+            str: The output of the reboot command.
+        """
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} shutdown.return"
+        return self._run_command(command)
+    
+    def stop_reboot(self):
+        """
+        Stops an ongoing reboot or shutdown process if it's within the offdelay time.
+
+        Returns:
+            str: The output of the stop reboot command.
+        """
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} shutdown.stop"
+        return self._run_command(command)
+    
+    def shutdown_delay(self):
+        """
+        Shuts down the UPS device after a defined delay (offdelay).
 
         Returns:
             str: The output of the shutdown command.
         """
         command = "sudo upsdrvctl shutdown"
+        return self._run_command(command)        
+
+    def shutdown_stayoff(self):
+        """
+        Shuts down the UPS device after a defined delay (shutdown_delay) and restarts it after another delay (restart_delay).
+        
+        Returns:
+            str: The output of the shutdown stay off command.
+        """
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} shutdown.stayoff"
         return self._run_command(command)
 
-    def reboot(self):
+    def power_off_load(self):
         """
-        Reboots the UPS device by sending the appropriate command.
+        Immediately turns off the load on the UPS.
 
         Returns:
-            str: The output of the reboot command.
+            str: The output of the power off load command.
         """
-        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} reboot.load"
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} load.off"
+        return self._run_command(command)
+
+    def power_on_load(self):
+        """
+        Immediately turns on the load on the UPS.
+
+        Returns:
+            str: The output of the power on load command.
+        """
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} load.on"
         return self._run_command(command)
 
     def get_status(self):
@@ -82,16 +149,6 @@ class UPSManager:
             str: The status of the UPS device.
         """
         command = f"upsc {self.ups_name}@localhost"
-        return self._run_command(command)
-
-    def list_commands(self):
-        """
-        Lists available commands for the UPS device.
-
-        Returns:
-            str: The list of available commands.
-        """
-        command = f"upscmd -l {self.ups_name}"
         return self._run_command(command)
 
     def get_input_voltage(self):
@@ -134,9 +191,19 @@ class UPSManager:
         command = f"upsc {self.ups_name}@localhost battery.charge"
         return self._run_command(command)
     
+    def battery_test(self):
+        """
+        Performs a quick battery test on the UPS device.
+
+        Returns:
+            str: The output of the battery test command.
+        """
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} test.battery.start.quick"
+        return self._run_command(command)
+
     def get_battery_voltage(self):
         """
-        Retrieves the battery volge level of the UPS device.
+        Retrieves the battery voltage level of the UPS device.
 
         Returns:
             str: The battery voltage level of the UPS device.
@@ -164,6 +231,17 @@ class UPSManager:
         command = f"upsc {self.ups_name}@localhost ups.status"
         return self._run_command(command)
 
+    def toggle_beeper(self):
+        """
+        Toggles the beeper on the UPS device.
+
+        Returns:
+            str: The output of the beeper toggle command.
+        """
+        command = f"sudo upscmd -u {self.user} -p {self.password} {self.ups_name} beeper.toggle"
+        return self._run_command(command)
+
+
 # Example usage
 if __name__ == "__main__":
     """
@@ -178,4 +256,4 @@ if __name__ == "__main__":
     print(ups_manager.get_battery_level())
     
     print("Shutdown UPS:")
-    print(ups_manager.shutdown())
+    print(ups_manager.shutdown_delay())
--- querryUPS.py
+++ querryUPS.py
@@ -1,5 +1,5 @@
 """
-Description: This script provides methods for controlling a UPS USB device on a linux system.
+Description: This script provides methods for controlling a UPS USB device on a Linux system.
 
 Author: Adrian Rabadan Ortiz
 
@@ -9,30 +9,46 @@ Date Modified: Month DD, Year
 
 Version: 1.0
 
-Dependencies: NUT
-
+Dependencies: UPSManager
 """
 
 from controlUPS import UPSManager
 import json
 import time
 
-ups_manager = UPSManager("Koblenz-7016", "admin", "admin_password")
+# Initialize UPSManager with UPS details
+ups_manager = UPSManager("koblenz-7016", "admin", "admin_password")
+
+def restartUPSServices(_print=0):
+    """
+    Restarts the UPS services (nut-server and nut-monitor).
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
+    if _print == 1:
+        print(ups_manager.restart_nut_services())
+    else:
+        ups_manager.restart_nut_services()
 
 def readUPS():
+    """
+    Reads various metrics from the UPS and returns them as a JSON string.
 
-    #Create a structured data dictionary
-    output_structure = { 
-
-        "BatL" : ups_manager.get_battery_level() + ' %',
-        "Vbat" : ups_manager.get_battery_voltage() + ' V',
-        "Vin"  :ups_manager.get_input_voltage() + ' V',
-        "Vout"  :ups_manager.get_output_voltage() + ' V',
-        "Mode" : ups_manager.get_ups_mode(),
-        "Freq" : ups_manager.get_frequency() + ' Hz'
+    Returns:
+        str: JSON string containing the UPS metrics.
+    """
+    # Create a structured data dictionary
+    output_structure = {
+        "BatL": ups_manager.get_battery_level() + '%',
+        "Vbat": ups_manager.get_battery_voltage() + 'V',
+        "Vin": ups_manager.get_input_voltage() + 'V',
+        "Vout": ups_manager.get_output_voltage() + 'V',
+        "Mode": ups_manager.get_ups_mode(),
+        "Freq": ups_manager.get_frequency() + 'Hz'
     }
 
-    # Reemplazar \n por espacio en cada valor del diccionario
+    # Replace \n with space in each value of the dictionary
     for key in output_structure:
         if isinstance(output_structure[key], str):
             output_structure[key] = output_structure[key].replace('\n', ' ').strip()
@@ -41,26 +57,110 @@ def readUPS():
 
     return json_output
 
-def shutDownUPS(_print=0):
+def restartDelay(_print=0):
+    """
+    Shuts down the UPS after a delay (see /etc/nut ups.conf [offdelay]) and restarts it after ups.delay.start
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
     if _print == 1:
-        print("Shutdown UPS after 30 secs:")
-        print(ups_manager.shutdown())
+        print("Shutting down UPS after [delayoff] seconds, please wait ...")
+        print(ups_manager.shutdown_delay())
     else:
-        ups_manager.shutdown()
+        ups_manager.shutdown_delay()
 
-def rebootUPS(_print=0):
+def powerOnLoad(_print=0):
+    """
+    Immediately turns on the load on the UPS.
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
     if _print == 1:
-        print("Rebooting UPS...")
-        print(ups_manager.reboot())
+        print("Powering on the UPS...")
+        print(ups_manager.power_on_load())
     else:
-        ups_manager.reboot()
-    
+        ups_manager.power_on_load()
 
-#print(ups_manager.get_status())
+def powerOffLoad(_print=0):
+    """
+    Immediately turns off the load on the UPS.
 
-reading = readUPS()
-print(reading)
-time.sleep(2)
-#shutDownUPS(1)
-#rebootUPS(1)
-print(ups_manager.list_commands())
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
+    if _print == 1:
+        print("Powering off the UPS...")
+        print(ups_manager.power_off_load())
+    else:
+        ups_manager.power_off_load()
+
+def rebootLoad(_print=0):
+    """
+    Reboots the UPS after a delay (offdelay) and restarts it again.
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
+    if _print == 1:
+        print("Rebooting UPS after [delayoff] sec and restarting again, please wait...")
+        print(ups_manager.reboot_load())
+    else:
+        ups_manager.reboot_load()
+
+def rebootStop(_print=0):
+    """
+    Stops an ongoing reboot or shutdown process if it's within the offdelay time.
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
+    if _print == 1:
+        print("Halting the UPS reboot...")
+        print(ups_manager.stop_reboot())
+    else:
+        ups_manager.stop_reboot()
+
+def batteryTest(_print=0):
+    """
+    Performs a quick battery test on the UPS.
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
+    if _print == 1:
+        print("Testing the UPS battery...")
+        print(ups_manager.battery_test())
+    else:
+        ups_manager.battery_test()
+
+def toggleBeeper(_print=0):
+    """
+    Toggles the beeper on the UPS.
+
+    Args:
+        _print (int, optional): If set to 1, prints the output of the command. Defaults to 0.
+    """
+    if _print == 1:
+        print("Toggling the UPS beeper...")
+        print(ups_manager.toggle_beeper())
+    else:
+        ups_manager.toggle_beeper()
+
+# Example usage of the functions
+if __name__ == "__main__":
+    """
+    Example usage of the UPSManager methods.
+    """
+    restartUPSServices(1)
+    time.sleep(5)
+    reading = readUPS()
+    print(reading)
+    time.sleep(2)
+    rebootLoad(1)
+    time.sleep(5)
+    rebootStop(1)
+    reading = readUPS()
+    print(reading)
+    time.sleep(2)
